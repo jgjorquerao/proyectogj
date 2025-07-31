3832ad9237b282fa69cd29f8dd11b7d9927cfd09
jgjorquerao <jgjorquerao@gmail.com>
2025-07-30 20:39:25 -0400

    Cambios en vista facts.blade y pricing.blade

--- resources/views/facts.blade.php
+++ resources/views/facts.blade.php
@@ -11,7 +11,7 @@
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
                 <div class="count-box">
                     <i class="bi bi-emoji-smile"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="56" data-purecounter-duration="1"
+                    <span data-purecounter-start="0" data-purecounter-end="9" data-purecounter-duration="1"
                         class="purecounter"></span>
                     <p><strong>Clientes felices</strong> han confiado en nosotros</p>
                 </div>
@@ -20,7 +20,7 @@
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
                 <div class="count-box">
                     <i class="bi bi-journal-richtext"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1"
+                    <span data-purecounter-start="0" data-purecounter-end="10" data-purecounter-duration="1"
                         class="purecounter"></span>
                     <p><strong>Proyectos</strong> exitosos hasta la fecha</p>
                 </div>
@@ -29,16 +29,16 @@
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
                 <div class="count-box">
                     <i class="bi bi-headset"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="350" data-purecounter-duration="1"
+                    <span data-purecounter-start="0" data-purecounter-end="248" data-purecounter-duration="1"
                         class="purecounter"></span>
-                    <p><strong>Horas de soporte</strong> tecnico y asesorías</p>
+                    <p><strong>Horas de soporte</strong> técnico y asesorías</p>
                 </div>
             </div>
 
             <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
                 <div class="count-box">
                     <i class="bi bi-people"></i>
-                    <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1"
+                    <span data-purecounter-start="0" data-purecounter-end="4" data-purecounter-duration="1"
                         class="purecounter"></span>
                     <p><strong>Trabajadores</strong> comprometidos con tus resultados</p>
                 </div>
